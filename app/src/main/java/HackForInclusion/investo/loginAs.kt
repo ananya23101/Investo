e1396d99a554f533c8465d5d67e3295a6cabab29
Anjuman Hasan <anju2084@gmail.com>
2022-05-15 00:36:49 +0530

    gradle files

---
+++ app/src/main/java/HackForInclusion/investo/loginAs.kt
@@ -0,0 +1,11 @@
+package HackForInclusion.investo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loginAs : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_as)
+    }
+}
