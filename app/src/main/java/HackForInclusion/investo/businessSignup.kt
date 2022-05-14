2b98b4f8647ebd287a2ab0b27864f5b15924481c
Anjuman Hasan <anju2084@gmail.com>
2022-05-15 01:04:20 +0530

    Added auth functions

---
+++ app/src/main/java/HackForInclusion/investo/businessSignup.kt
@@ -0,0 +1,11 @@
+package HackForInclusion.investo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class businessSignup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_business_signup)
+    }
+}
---
+++ app/src/main/java/HackForInclusion/investo/login.kt
@@ -0,0 +1,11 @@
+package HackForInclusion.investo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
